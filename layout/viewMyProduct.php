707f26377ea2fae42efe7e15719353d5f5e94f97
Han0910 <s3894290@rmit.edu.vn>
2022-08-26 15:20:52 +0700

    shipper register, add new product done, view my product on progress

--- layout/viewMyProduct.php
+++ layout/viewMyProduct.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -12,15 +12,24 @@
   </head>
   <body>
     <header class="col-12 p-0">
-        <div>
-            <div class="header_vendor">
-                <div class="color_overlay d-flex justify-content-center align-items-center">
-                    <div>
-                        <h3>DASHBOARD</h3>
-                    </div>
+        <div class="header_vendor">
+            <div class="color_overlay d-flex justify-content-center align-items-center">
+                <div>
+                    <h3>DASHBOARD</h3>
                 </div>
             </div>
         </div>
     </header>
+    <div class='container bg-primary mt-4'>
+      <div class='row justify-content-center'>
+            <div class='mb-4'>
+                <h2 class="col-12 text-center ">ORDER</h2>
+            </div>
+            <div class="col-lg-10 bg-danger">
+              <?= make_tbl($data) ?>
+            </div>
+    </div>
+    
+
   </body>
 </html>
