542f9d67610f8d8ff171208fe2b912ed002c564f
Vo Thanh Hoang Anh <alaine210902@gmail.com>
2022-08-29 17:13:35 +0700

    Add class product

--- www/assets/js/register.js
+++ www/assets/js/register.js
@@ -1,3 +1,4 @@
+/* Register Validation For Client-side */
 const form = document.getElementById('form');
 const username = document.getElementById('form');
 const password = document.getElementById('form');
@@ -13,4 +14,22 @@ function checkInputs() {
     // get the input values
     const usernameValue = username.value.trim();
     const passwordValue = password.value.trim();
+
+    if (usernameValue === '') {
+        // display error and add error class
+        setErrorFor(username, 'Username cannot be blank');
+    } else {
+        setSuccessFor(username);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement; // this is the .form-control class
+    const small = formControl.querySelector('small');
+
+    // display error message inside small
+    small.innerText = message;
+
+    // add error class
+    formControl.className = 'form-control-error';
 }
