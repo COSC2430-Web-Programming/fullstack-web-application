ec28ad3911a7d5297a50a2f056008c9b08d6f882
sangnguyen0403 <sangnguyen04032002@gmail.com>
2022-08-29 16:21:50 +0700

    Create order class

---
+++ app/class/order.php
@@ -0,0 +1,14 @@
+<?php 
+    class Order{
+        protected $orderID;
+        protected $products;
+        protected $total_price;
+        protected $address;
+        protected $status;
+        public $error;
+        public $success;
+        protected $storage = "../../database"
+        
+
+    }
+?>
