3bcc15281dc6ed65b1ca8fe335dcf2263376dbd3
Han0910 <s3894290@rmit.edu.vn>
2022-08-31 10:49:17 +0700

    Done UI view my product

--- app/pages/vendor/viewMyProduct.php
+++ app/pages/vendor/viewMyProduct.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="../../../www/assets/css/style.css" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -14,7 +14,7 @@
   <header class='col-12 p-0'>
       <div class="container">
         <?php 
-          require('nav.php')
+          require('../layout/nav.php')
         ?>
 
       </div>
