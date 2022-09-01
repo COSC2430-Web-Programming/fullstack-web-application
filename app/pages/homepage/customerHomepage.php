e8fb88617365b3cc17e01fd680be03c18c1c1b3c
Han0910 <s3894290@rmit.edu.vn>
2022-09-02 02:17:56 +0700

    Load data for customer homepage done

--- app/pages/homepage/customerHomepage.php
+++ app/pages/homepage/customerHomepage.php
@@ -24,58 +24,36 @@
       </div>
     </header>
     <main>
-    <div class='container mt-4'>
-        <div class='row justify-content-center'>
-            <div class='mb-4'>
-                <h2 class="col-12 text-center">ALL PRODUCTS</h2>
-            </div>
-            <div class="row justify-content-evenly">
-                <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 card">  
-                    <img src="http://cdn.tgdd.vn/Files/2020/10/24/1301635/list-12-nha-hang-quan-an-sushi-cuc-chat-luong-o-quan-1-202201141555392974.jpg" class='card-img-top'>
-                    <div class="card-body d-flex justify-content-between ml-xl-3">
-                        <span class='fw-bold'>Sushi</span>
-                        <span class='fw-semibold'>100$</span>
-                    </div>
+        <div class='container mt-4'>
+            <div class='row justify-content-center'>
+                <div class='mb-4'>
+                    <h2 class="col-12 text-center">ALL PRODUCTS</h2>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 card">  
-                    <img src="http://cdn.tgdd.vn/Files/2020/10/24/1301635/list-12-nha-hang-quan-an-sushi-cuc-chat-luong-o-quan-1-202201141555392974.jpg" class='card-img-top'>
-                    <div class="card-body d-flex justify-content-between">
-                        <span class='fw-bold'>Sushi</span>
-                        <span class='fw-semibold'>100$</span>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 card">  
-                    <img src="http://cdn.tgdd.vn/Files/2020/10/24/1301635/list-12-nha-hang-quan-an-sushi-cuc-chat-luong-o-quan-1-202201141555392974.jpg" class='card-img-top'>
-                    <div class="card-body d-flex justify-content-between">
-                        <span class='fw-bold'>Sushi</span>
-                        <span class='fw-semibold'>100$</span>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 card">  
-                    <img src="http://cdn.tgdd.vn/Files/2020/10/24/1301635/list-12-nha-hang-quan-an-sushi-cuc-chat-luong-o-quan-1-202201141555392974.jpg" class='card-img-top'>
-                    <div class="card-body d-flex justify-content-between">
-                        <span class='fw-bold'>Sushi</span>
-                        <span class='fw-semibold'>100$</span>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 card">  
-                    <img src="http://cdn.tgdd.vn/Files/2020/10/24/1301635/list-12-nha-hang-quan-an-sushi-cuc-chat-luong-o-quan-1-202201141555392974.jpg" class='card-img-top'>
-                    <div class="card-body d-flex justify-content-between">
-                        <span class='fw-bold'>Sushi</span>
-                        <span class='fw-semibold'>100$</span>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 card">  
-                    <img src="http://cdn.tgdd.vn/Files/2020/10/24/1301635/list-12-nha-hang-quan-an-sushi-cuc-chat-luong-o-quan-1-202201141555392974.jpg" class='card-img-top'>
-                    <div class="card-body d-flex justify-content-between ">
-                        <span class='fw-bold'>Sushi</span>
-                        <span class='fw-semibold'>100$</span>
-                    </div>
-                </div>
-                
+                <div class="row justify-content-evenly">
+                <?php
+                    session_start();
+                    $json_data = file_get_contents("../../database/products.db");
+                    $products = json_decode($json_data,true);
+                    foreach ($products as $product){
+                        ?>
+                        <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 card">
+                            <img src='<?php echo "../../../www/assets/images/".$product['image'] ?>' class='card-img-top'>
+                            <div class="card-body d-flex justify-content-between ml-xl-3">
+                            <span class='fw-bold'><?php echo $product['name']?></span>
+                            <span class='fw-semibold'><?php echo $product['price']?></span>
+                            </div>
+                        </div>
+                    <?php
+                    }
+                ?>
             </div>
         </div>
     </main>
+    <footer>
+        <?php 
+            require('../layout/footer.php')
+        ?>
+    </footer>
 
 
 
--- app/pages/vendor/viewMyProduct.php
+++ app/pages/vendor/viewMyProduct.php
@@ -26,33 +26,36 @@
         </div>
       </div>
     </header>
-    <div class='container mt-4'>
-      <div class='row justify-content-center'>
-            <div class='mb-4'>
-                <h2 class="col-12 text-center">ALL PRODUCTS</h2>
-            </div>
-            <div class="row justify-content-evenly">
-            <?php
-              session_start();
-              $json_data = file_get_contents("../../database/products.db");
-              $products = json_decode($json_data,true);
-              foreach ($products as $product){
-                if (strcmp($_SESSION['user'], $product['creator']) == 0){
-                  ?>
-                    <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 card">
-                      <img src='<?php echo "../../../www/assets/images/".$product['image'] ?>' class='card-img-top'>
-                      <div class="card-body d-flex justify-content-between ml-xl-3">
-                        <span class='fw-bold'><?php echo $product['name']?></span>
-                        <span class='fw-semibold'><?php echo $product['price']?></span>
-                      </div>
-                    </div>
-              
-                <?php
-                }
-              }
-              ?>
+    <main>
+      <div class='container mt-4'>
+        <div class='row justify-content-center'>
+              <div class='mb-4'>
+                  <h2 class="col-12 text-center">ALL PRODUCTS</h2>
               </div>
-    </div>
+              <div class="row justify-content-evenly">
+              <?php
+                session_start();
+                $json_data = file_get_contents("../../database/products.db");
+                $products = json_decode($json_data,true);
+                foreach ($products as $product){
+                  if (strcmp($_SESSION['user'], $product['creator']) == 0){
+                    ?>
+                      <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 card">
+                        <img src='<?php echo "../../../www/assets/images/".$product['image'] ?>' class='card-img-top'>
+                        <div class="card-body d-flex justify-content-between ml-xl-3">
+                          <span class='fw-bold'><?php echo $product['name']?></span>
+                          <span class='fw-semibold'><?php echo $product['price']?></span>
+                        </div>
+                      </div>
+                
+                  <?php
+                  }
+                }
+             ?>
+         </div>
+      </div>
+    </main>
+    
     <footer class='mt-4'>
       <?php 
           require('../layout/footer.php')
