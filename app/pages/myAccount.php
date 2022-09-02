c1d7ffa944eebb714c5dd346c97f82d364c6ddba
Han0910 <s3894290@rmit.edu.vn>
2022-09-02 19:01:39 +0700

    Doing my account page

--- app/pages/myAccount.php
+++ app/pages/myAccount.php
@@ -1,5 +1,10 @@
 <?php
-  session_start()
+  session_start();
+  define('VENDOR_ROLE', 0);
+  define('CUSTOMER_ROLE', 1);
+  define('SHIPPER_ROLE', 2);
+  $json_data = file_get_contents("../database/accounts.db");
+  $accounts = json_decode($json_data, true);
 ?>
 <!doctype html>
 
@@ -36,14 +41,9 @@
                 <div class='mb-4'>
                     <h2 class="col-12 text-center ">MY ACCOUNT</h2>
                 </div>
-                <div class="col-10">
+                <div class="col-12">
                     <ul class='d-flex justify-content-around list-unstyled text-center'>
                     <?php
-                        define('VENDOR_ROLE', 0);
-                        define('CUSTOMER_ROLE', 1);
-                        define('SHIPPER_ROLE', 2);
-                        $json_data = file_get_contents("../database/accounts.db");
-                        $accounts = json_decode($json_data, true);
                         foreach($accounts as $account){
                           if(strcmp($_SESSION['user'], $account['username']) == 0){
                             if($account['role'] == VENDOR_ROLE){
@@ -72,30 +72,60 @@
                         ?>
                     </ul>
                     <?php
-                      foreach($GLOBALS('accounts') as $account){
+                      foreach($accounts as $account){
                         if(strcmp($_SESSION['user'], $account['username']) == 0){
                           ?>
-                           <div class="mb-4">
-                            <label for="customerName" class="form-label pb-3 ">Username</label>
-                            <label for="customerName" class="form-control w-100" id="customerName"><?php echo $account['username']?></label>
-                    </div>
+                          <div class="row mt-4 justify-content-evenly">
+                            <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 text-center mb-5">
+                            <!-- <input name="customerProfile" type="file" class="form-control w-100" id="customerProfile"> -->
+                            <img src='<?php echo "../../www/assets/images/".$account['profilePicture'] ?>' width="60% ">
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 info">
+                              <div class="mb-4">
+                                <label for="customerName" class="form-label pb-3 ">Username</label>
+                                <label for="customerName" class="form-control w-100" id="customerName"><?php echo $account['username']?></label>
+                              </div>
+                              <div class="mb-4">
+                                <label for="customerName" class="form-label pb-3 ">Name</label>
+                                <label for="customerName" class="form-control w-100" id="customerName"><?php echo $account['name'] ?></label>
+                              </div>
+                  
+                              <?php
+                                foreach($accounts as $account){
+                                  if(strcmp($_SESSION['user'], $account['username']) == 0){
+                                    if($account['role'] == VENDOR_ROLE){
+                                        ?>
+                                      <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Vendor</li>
+                                    <?php
+                                      }
+                                    ?>
+                                    <?php 
+                                    if($account['role'] == CUSTOMER_ROLE){
+                                      ?>
+                                      <div class="mb-5">
+                                        <label for="customerAddress" class="form-label pb-2">Address</label>
+                                        <label for="customerAddress" class="form-control w-100" id="customerAddress"><?php echo $account['address'] ?></label>
+                                      </div>
+                                    <?php
+                                      }
+                                    ?>
+                                    <?php 
+                                    if($account['role'] == SHIPPER_ROLE){
+                                      ?>
+                                      <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Shipper</li>
+                                    <?php
+                                      }
+                                    ?>
+                                <?php
+                                  }
+                                }
+                                ?>
+                              </div>
+                          </div>
                     <?php
                         }
                       }
                     ?>
-                   
-                    <div class="mb-4">
-                        <label for="customerName" class="form-label pb-3 ">Username</label>
-                        <label for="customerName" class="form-control w-100" id="customerName">maytran216</label>
-                    </div>
-                    <div class="mb-5">
-                        <label for="customerProfile" class="form-label pb-2">Profile picture</label>
-                        <input name="customerProfile" type="file" class="form-control w-100" id="customerProfile">
-                    </div>
-                    <div class="mb-5">
-                        <label for="customerAddress" class="form-label pb-2">Address</label>
-                        <label for="customerAddress" class="form-control w-100" id="customerAddress">702 Nguyen Van Linh</label>
-                    </div>
                     <div class="mb-4 d-grid gap-2 col-6 mx-auto">
                         <ul class = 'd-flex justify-content-around list-unstyled text-center'>
                             <li input name='submit' value="Change Password" type="submit" class=" btn btn-outline-dark " id="submit">Change Password
