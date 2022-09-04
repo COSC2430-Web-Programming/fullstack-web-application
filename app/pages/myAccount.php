ed364e5c40567252c8ca9efc46a14e1c49182b99
Han0910 <s3894290@rmit.edu.vn>
2022-09-04 14:24:22 +0700

    Doing logout and update

--- app/pages/myAccount.php
+++ app/pages/myAccount.php
@@ -41,28 +41,28 @@
                 <div class='mb-4'>
                     <h2 class="col-12 text-center ">MY ACCOUNT</h2>
                 </div>
-                <div class="col-12">
-                    <ul class='d-flex justify-content-around list-unstyled text-center'>
+                <div class="col-10">
+                  <ul class='d-flex justify-content-around list-unstyled text-center'>
                     <?php
                         foreach($accounts as $account){
                           if(strcmp($_SESSION['user'], $account['username']) == 0){
                             if($account['role'] == VENDOR_ROLE){
                                 ?>
-                              <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Vendor</li>
+                              <li class=' col-xl-2 col-lg-2 col-md-2 m-2 p-2 bg-secondary border border-secondary'>Vendor</li>
                             <?php
                               }
                             ?>
                             <?php 
                             if($account['role'] == CUSTOMER_ROLE){
                               ?>
-                              <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Customer</li>
+                              <li class='col-xl-2 col-lg-2 col-md-2 m-2 p-2 bg-secondary border border-secondary'>Customer</li>
                             <?php
                               }
                             ?>
                             <?php 
                             if($account['role'] == SHIPPER_ROLE){
                               ?>
-                              <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Shipper</li>
+                              <li class='col-xl-2 col-lg-2 col-md-2 m-2 p-2 bg-secondary border border-secondary'>Shipper</li>
                             <?php
                               }
                             ?>
@@ -76,18 +76,14 @@
                         if(strcmp($_SESSION['user'], $account['username']) == 0){
                           ?>
                           <div class="row mt-4 justify-content-evenly">
-                            <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 text-center mb-5">
+                            <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 text-center mb-2">
                             <!-- <input name="customerProfile" type="file" class="form-control w-100" id="customerProfile"> -->
-                            <img src='<?php echo "../../www/assets/images/".$account['profilePicture'] ?>' width="60% ">
+                            <img class='avatar' src='<?php echo "../../www/assets/images/".$account['profilePicture'] ?>'>
                             </div>
-                            <div class="col-xl-4 col-lg-4 col-md-6 col-md-12 info">
+                            <div class="col-xl-4 col-lg-4 col-md-6 col-md-12">
                               <div class="mb-4">
                                 <label for="customerName" class="form-label pb-3 ">Username</label>
                                 <label for="customerName" class="form-control w-100" id="customerName"><?php echo $account['username']?></label>
-                              </div>
-                              <div class="mb-4">
-                                <label for="customerName" class="form-label pb-3 ">Name</label>
-                                <label for="customerName" class="form-control w-100" id="customerName"><?php echo $account['name'] ?></label>
                               </div>
                   
                               <?php
@@ -95,14 +91,25 @@
                                   if(strcmp($_SESSION['user'], $account['username']) == 0){
                                     if($account['role'] == VENDOR_ROLE){
                                         ?>
-                                      <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Vendor</li>
+                                      <div class="mb-4">
+                                        <label for="customerAddress" class="form-label pb-2">Business Name</label>
+                                        <label for="customerAddress" class="form-control w-100" id="customerAddress"><?php echo $account['businessName'] ?></label>
+                                      </div>
+                                      <div class="mb-4">
+                                        <label for="customerAddress" class="form-label pb-2">Business Address</label>
+                                        <label for="customerAddress" class="form-control w-100" id="customerAddress"><?php echo $account['businessAddress'] ?></label>
+                                      </div>
                                     <?php
                                       }
                                     ?>
                                     <?php 
                                     if($account['role'] == CUSTOMER_ROLE){
                                       ?>
-                                      <div class="mb-5">
+                                      <div class="mb-4">
+                                        <label for="customerName" class="form-label pb-3 ">Name</label>
+                                        <label for="customerName" class="form-control w-100" id="customerName"><?php echo $account['name'] ?></label>
+                                      </div>
+                                      <div class="mb-4">
                                         <label for="customerAddress" class="form-label pb-2">Address</label>
                                         <label for="customerAddress" class="form-control w-100" id="customerAddress"><?php echo $account['address'] ?></label>
                                       </div>
@@ -112,7 +119,10 @@
                                     <?php 
                                     if($account['role'] == SHIPPER_ROLE){
                                       ?>
-                                      <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Shipper</li>
+                                      <div class="mb-4">
+                                        <label for="customerAddress" class="form-label pb-2">Distribution Hub</label>
+                                        <label for="customerAddress" class="form-control w-100" id="customerAddress"><?php echo $account['distributionHub'] ?></label>
+                                      </div>
                                     <?php
                                       }
                                     ?>
@@ -126,12 +136,10 @@
                         }
                       }
                     ?>
-                    <div class="mb-4 d-grid gap-2 col-6 mx-auto">
-                        <ul class = 'd-flex justify-content-around list-unstyled text-center'>
-                            <li input name='submit' value="Change Password" type="submit" class=" btn btn-outline-dark " id="submit">Change Password
-                            <li input name='submit' value="Log Out" type="submit" class=" btn btn-outline-dark " id="submit">Log Out
-                        </ul>
-                    </div>
+                      <ul class = 'd-flex justify-content-evenly list-unstyled text-center'>
+                          <li class=" btn btn-outline-dark " >Update Profile Picture</li>
+                          <li class="myacc_btn btn btn-outline-dark" onclick="location.href='logout.php';">Log Out</li>
+                      </ul>
                 </div>
             </div>
         </div>
@@ -143,26 +151,3 @@
   </footer>
   </body>
 </html>
-
-<!-- <?php
-                        session_start();
-                        if($_SESSION('user') === 0){
-                          ?>
-                        <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Vendor</li>
-                      <?php
-                        }
-                      ?>
-                      <?php 
-                        if($_SESSION('user')['role'] === 1){
-                          ?>
-                        <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Customer</li>
-                      <?php
-                        }
-                      ?>
-                      <?php 
-                        if($_SESSION('user')['role'] === 2){
-                          ?>
-                        <li class=' col-lg-2 col-md m-2 p-2 bg-secondary border border-secondary'>Shipper</li>
-                      <?php
-                        }
-                      ?> -->
