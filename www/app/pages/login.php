6a3152d163ea08df100fe12244b79bf57a7e8ba7
Vo Thanh Hoang Anh <alaine210902@gmail.com>
2022-09-09 22:51:33 +0700

    fix logo link in header

--- www/app/pages/login.php
+++ www/app/pages/login.php
@@ -59,13 +59,12 @@ if (isset($_POST['submit'])) {
             </section>
         </div>
     </main>
+    <div style="height: 145px"></div>
     <footer>
         <?php 
           require('layout/footer.php')
       ?>
     </footer>
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
