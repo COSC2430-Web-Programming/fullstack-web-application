bedf6e6e558b896ff2cac1c3cbe2ef75606fff12
sangnguyen0403 <sangnguyen04032002@gmail.com>
2022-09-09 22:32:20 +0700

    Add footer

--- www/app/pages/login.php
+++ www/app/pages/login.php
@@ -59,6 +59,11 @@ if (isset($_POST['submit'])) {
             </section>
         </div>
     </main>
+    <footer>
+        <?php 
+          require('layout/footer.php')
+      ?>
+    </footer>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
